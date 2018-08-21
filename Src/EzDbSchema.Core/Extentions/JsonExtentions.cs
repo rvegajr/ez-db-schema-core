26f5497b9598aa5e862da98cd794d54d7ccc551c
Ricardo Vega <github@noctusoft.com>
2018-08-21 13:00:38 -0600

    Added fix for slashes in app settings JSON

--- Src/EzDbSchema.Core/Extentions/JsonExtentions.cs
+++ Src/EzDbSchema.Core/Extentions/JsonExtentions.cs
@@ -6,10 +6,11 @@ namespace EzDbSchema.Core.Extentions.Json
     {
         private const string DOUBLE_QUOTE_SUB = @"_$$_";
         private const string DOUBLE_QUOTE = @"""""";
+        private const string DOUBLE_SLASH = @"\\";
 
         public static string AsString(this JsonValue obj)
         {
-            return obj.ToString().Replace(DOUBLE_QUOTE, DOUBLE_QUOTE_SUB).Replace("\"", "").Replace(DOUBLE_QUOTE_SUB, "\"");
+            return obj.ToString().Replace(DOUBLE_QUOTE, DOUBLE_QUOTE_SUB).Replace("\"", "").Replace(DOUBLE_QUOTE_SUB, "\"").Replace(DOUBLE_SLASH, @"\");
         }
     }
 }
