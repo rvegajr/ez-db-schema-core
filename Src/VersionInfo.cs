454a555219f973baef7d3c9d758531b682ff91da
Ricky Vega <git@noctusoft.com>
2020-12-13 19:56:14 -0600

    Updated to .net 5

--- Src/VersionInfo.cs
+++ Src/VersionInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.23.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.23.0")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.23.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
